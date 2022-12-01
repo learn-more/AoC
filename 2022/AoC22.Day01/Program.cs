f48e98c02796117012f13284c10961902260a079
Mark Jansen <mark.jansen@reactos.org>
2022-12-01 19:29:40 +0100

    Add Day1

---
+++ 2022/AoC22.Day01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+
+static string GetSourceFilePathName([CallerFilePath] string? callerFilePath = null) => callerFilePath ?? string.Empty;
+string[] input = File.ReadAllLines(Path.Combine(Path.GetDirectoryName(GetSourceFilePathName()) ?? string.Empty, "input.txt"));
+
+static IEnumerable<int> FindMax(string[] inputs, int max_count)
+{
+    List<int> counts = new();
+    int total = 0;
+    foreach (string line in inputs.Select(elem => elem.Trim()))
+    {
+        if (string.IsNullOrEmpty(line))
+        {
+            counts.Add(total);
+            total = 0;
+        }
+        else
+        {
+            total += int.Parse(line);
+        }
+    }
+    counts.Add(total);
+    return counts.OrderByDescending(e => e).Take(max_count);
+}
+
+Console.WriteLine($"Max(1): {FindMax(input, 1).Sum()}");
+Console.WriteLine($"Max(3): {FindMax(input, 3).Sum()}");
+
---
+++ 2022/gen.bat
@@ -0,0 +1,3 @@
+
+dotnet new console -n "AoC22.Day02"
+dotnet sln add "AoC22.Day02"
